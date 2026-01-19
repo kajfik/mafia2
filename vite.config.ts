fad71863b8d485d782a0c8625ddddcf4e11963b9
kajfik <kajf.jakub@gmail.com>
2026-01-19 11:57:26 +0100

    Move base path config to top-level in Vite config
    Relocates the 'base' property from the server section to the top-level of the Vite config for correct deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mafia/', // for GitHub Pages
   server: {
     host: true,   // exposes to your LAN
-    base: '/mafia/', // for GitHub Pages
   },
 })
