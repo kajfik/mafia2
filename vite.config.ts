7b44966e51775baac33cb2bd5fdf451389a42f1a
kajfik <kajf.jakub@gmail.com>
2026-01-19 12:05:39 +0100

    Update Vite base path for GitHub Pages
    Changed the Vite config base path from '/mafia/' to '/mafia2/' to reflect the new deployment location on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mafia/', // for GitHub Pages
+  base: '/mafia2/', // for GitHub Pages
   server: {
     host: true,   // exposes to your LAN
   },
